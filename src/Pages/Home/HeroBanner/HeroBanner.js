e789c71f67d804f7a93124ea4e3a050698a99e61
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-12-12 01:01:15 +0800

    added project section

--- src/Pages/Home/HeroBanner/HeroBanner.js
+++ src/Pages/Home/HeroBanner/HeroBanner.js
@@ -2,24 +2,19 @@ import React from "react";
 
 const HeroBanner = () => {
   return (
-    <section class="bg-gray-900">
+    <section class="bg-gray-900 ">
       <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
-      <span class="text-sm font-medium text-green-400 text-gradient-to-r from-pink-500 to-orange-600">
+        <div class="mx-auto max-w-screen-sm text-center mb-8 lg:mb-16">
+          <span class="text-sm font-medium text-green-400 text-gradient-to-r from-pink-500 to-orange-600">
             React Js Developer | MERN Stack Developer
           </span>
-        <h1 class="mb-4 text-3xl uppercase font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r to-orange-500 from-pink-500">
-          Rejaul Karim Rabbi
-        </h1>
-        
-       <span>
-       <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
-          Here at Flowbite we focus on markets where technology,
-          <br />
-           innovation, and
-          capital can unlock long-term value and drive economic growth.
-        </p>
-       </span>
-        <div className="mt-8">
+          <h2 class="mb-4 text-3xl uppercase font-extrabold text-gray-900 dark:text-white md:text-4xl lg:text-5xl text-transparent bg-clip-text bg-gradient-to-r to-orange-500 from-pink-500">
+            Rejaul Karim Rabbi
+          </h2>
+          <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+            Explore the whole collection of open-source web components and
+            elements built with the utility classes from Tailwind
+          </p>
           <button
             type="button"
             class="text-white bg-gradient-to-r from-pink-500 to-orange-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-500 dark:focus:ring-orange-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroBanner from '../HeroBanner/HeroBanner';
+import Projects from '../Projects/Projects';
 
 const Home = () => {
     return (
         <>
             <HeroBanner/>
+            <Projects/>
         </>
     );
 };
