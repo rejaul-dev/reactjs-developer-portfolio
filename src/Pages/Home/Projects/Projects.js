2d2a592271aafcfb172a55da3843b588fd6b7278
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-12-12 20:37:00 +0800

    added portfolio projects

--- src/Pages/Home/Projects/Projects.js
+++ src/Pages/Home/Projects/Projects.js
@@ -7,48 +7,49 @@ const Projects = () => {
       id: 1,
       name: "Doctor's Portal",
       descriptions: "Full Stack Web Application",
-      img: "",
+      img: "https://i.ibb.co/f8hg8jk/Screenshot-2022-12-12-203149.png",
       live_site: "https://doctors-portal-d899f.web.app/",
-      github_repo: "",
+      github_client: "https://github.com/rejaul-karim10/doctors-portal-client",
+      github_server:"https://github.com/rejaul-karim10/doctors-portal-server",
     },
     {
       id: 1,
-      name: "Dragon News",
+      name: "Laptop Hunter",
       descriptions: "Full Stack Web Application",
-      img: "",
-      live_site: "",
-      github_repo: "",
+      img: "https://i.ibb.co/X8d22rJ/Screenshot-2022-12-12-203351.png",
+      live_site: "https://resell-used-laptop.web.app/",
+      github_client: "https://github.com/rejaul-karim10/resell-used-laptop-client",
+      github_server:"https://github.com/rejaul-karim10/resell-used-laptop-server"
     },
     {
       id: 1,
       name: "Cloud Kitchen",
       descriptions: "Full Stack Web Application",
-      img: "",
-      live_site: "",
-      github_repo: "",
+      img: "https://i.ibb.co/dm3Zm4w/Screenshot-2022-12-12-202637.png",
+      live_site: "https://cloud-kitchen-57804.web.app",
+      github_client: "https://github.com/rejaul-karim10/cloud-kitchen",
+      github_server: "https://github.com/rejaul-karim10/cloud-kitchen-server"
     },
   ];
 
   return (
     <section id="portfolio" className="mt-24">
-      <div>
-        <div className="mx-auto max-w-screen-sm text-center mb-4">
-          <h2 className="mb-4 text-2xl font-extrabold text-primary uppercase">
-            {" "}
-            My Projects
-          </h2>
-          <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-            Explore the whole collection of open-source web components and
-            elements built with the utility classes from Tailwind
-          </p>
-        </div>
-        <div className="grid grid-clos-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((project) => (
-            <ProjectsCard key={project.id} project={project} />
-          ))}
-        </div>
-        {/* <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">View More</button> */}
+      <div className="mx-auto max-w-screen-sm text-center mb-4">
+        <h2 className="mb-4 text-2xl font-extrabold text-primary uppercase">
+          {" "}
+          My Projects
+        </h2>
+        <p className="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
+          Explore the whole collection of open-source web components and
+          elements built with the utility classes from Tailwind
+        </p>
       </div>
+      <div className="grid grid-clos-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {projects.map((project) => (
+          <ProjectsCard key={project.id} project={project} />
+        ))}
+      </div>
+      {/* <button className="btn btn-primary bg-gradient-to-r from-primary to-secondary">View More</button> */}
     </section>
   );
 };
--- src/Pages/Home/Projects/ProjectsCard.js
+++ src/Pages/Home/Projects/ProjectsCard.js
@@ -4,19 +4,20 @@ const ProjectsCard = ({ project }) => {
   const { name, descriptions, img, live_site, github_client, github_server } =
     project;
   return (
-    <div className="card w-96 bg-gray-800 shadow-xl">
+    <div className="card bg-gray-800 shadow-xl">
       <figure>
-        <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+        <img src={img} alt="Shoes" />
       </figure>
       <div className="card-body">
-        <h2 className="card-title text-primary">
+        <h2 className="card-title text-gray-400">
           {name}
           <div className="badge badge-success text-white">NEW</div>
         </h2>
         <p>{descriptions}</p>
         <div className="grid grid-cols-3 gap-2">
-          <a href={live_site} target="_blank" rel="noreferrer" className="badge badge-outline p-4">View Live</a>
-          
+          <a href={live_site} target="_blank" rel="noreferrer" className="btn btn-xs btn-primary">View Live</a>
+          <a href={github_client} target="_blank" rel="noreferrer" className="btn btn-xs btn-primary">Client</a>
+          <a href={github_server} target="_blank" rel="noreferrer" className="btn btn-xs btn-primary">Server</a>
         </div>
       </div>
     </div>
